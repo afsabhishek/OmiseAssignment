2d2ba3337308919cb4c988c910ecc57836a3fb0a
akumar <a.kumar@mylifemyfinance.com.au>
2019-12-24 02:59:51 +0530

    View designed and test with API data

--- app/src/main/java/com/example/omisetest/presentation/BindingRecyclerAdapter.kt
+++ app/src/main/java/com/example/omisetest/presentation/BindingRecyclerAdapter.kt
@@ -1,14 +1,22 @@
 package com.example.omisetest.presentation
 
+import android.support.v7.widget.AppCompatImageView
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.omisetest.R
+import com.example.omisetest.presentation.charity.presentation.CharityViewModel
+import kotlinx.android.synthetic.main.view_charity.view.*
 
 abstract class BindingRecyclerAdapter<T>(
   private val onClick: OnClick<T>
 ) : RecyclerView.Adapter<BindingViewHolder<T>>(), ViewHolderBinder<T> {
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<T> = buildViewHolder(parent, viewType)
-  override fun onBindViewHolder(holder: BindingViewHolder<T>, position: Int) = bindViewHolder(holder, position, onClick)
+  override fun onBindViewHolder(holder: BindingViewHolder<T>, position: Int) {
+    bindViewHolder(holder, position, onClick)
+    Glide.with(holder.itemView.context).load((getItemForPosition(position) as CharityViewModel.CharityDetails).logoUrl).into(holder.itemView.findViewById<AppCompatImageView>(R.id.logoUrl))
+  }
 }
 
   class RecyclerViewListAdapter<T : Any>(
--- app/src/main/java/com/example/omisetest/presentation/CharityViewModel.kt
+++ app/src/main/java/com/example/omisetest/presentation/CharityViewModel.kt
@@ -1,7 +1,22 @@
 package com.example.omisetest.presentation
 
+import android.databinding.BindingAdapter
+import android.support.v7.widget.AppCompatImageView
+import com.bumptech.glide.Glide
+
 open class CharityViewModel(
   open val name: String,
   open val logoUrl: String,
   open val id: String
-)
+) {
+
+  companion object {
+
+    @JvmStatic
+    @BindingAdapter("bind:logoUrl")
+    fun loadImage(view: AppCompatImageView, logoUrl: String) { // This methods should not have any return type, = declaration would make it return that object declaration.
+      Glide.with(view.context).load(logoUrl).into(view)
+    }
+  }
+
+}
