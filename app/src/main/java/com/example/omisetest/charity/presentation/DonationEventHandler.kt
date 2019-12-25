eb47a543e03536f24902a0146e924fd5b5c3d870
akumar <a.kumar@mylifemyfinance.com.au>
2019-12-25 10:38:19 +0530

    Added Card UI

--- app/src/main/java/com/example/omisetest/charity/presentation/DonationEventHandler.kt
+++ app/src/main/java/com/example/omisetest/charity/presentation/DonationEventHandler.kt
@@ -1,12 +1,19 @@
 package com.example.omisetest.charity.presentation
 
 import android.app.Activity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import com.example.omisetest.Configuration
 import com.example.omisetest.Interactor
 import com.example.omisetest.presentation.charity.presentation.CharityViewModel
 
 class DonationEventHandler(
-  context: Activity
+  context: Activity,
+  cardNumber: EditText,
+  cvv: EditText,
+  expiryDate: EditText,
+  cardHolder: EditText
 ) : Interactor(context) {
 
   private val selectedCharity = Configuration.deps.useCase.charity.getBrowseCharity
@@ -14,6 +21,55 @@ class DonationEventHandler(
   var charityViewModel: CharityViewModel.CharityDetails? = null
 
   init {
-      charityViewModel = selectedCharity.request()
+    charityViewModel = selectedCharity.request()
+  }
+
+  val cardNumberWatcher = object : TextWatcher {
+    private val space = ' '
+    private val numbers = "0123456789"
+
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+    }
+
+    override fun afterTextChanged(s: Editable) {
+      var pos = 0
+      while (true) {
+        if (pos >= s.length) break
+        if (space === s[pos] && ((pos + 1) % 5 != 0 || pos + 1 == s.length)) {
+          s.delete(pos, pos + 1)
+        } else {
+          pos++
+        }
+      }
+
+      pos = 4
+      while (true) {
+        if (pos >= s.length) break
+        val c = s[pos]
+        if (numbers.indexOf(c) >= 0) {
+          s.insert(pos, "" + space)
+        }
+        pos += 5
+      }
+    }
+  }
+
+  val expiryWatcher = object : TextWatcher {
+
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+
+    override fun afterTextChanged(s: Editable) {
+      if (s.length > 4) {
+        return
+      }
+      if (s.length > 2 && s.toString()[2] != '/') {
+        s.insert(2, "/")
+      }
+    }
   }
 }
--- app/src/main/java/com/example/omisetest/charity/presentation/DonationFragment.kt
+++ app/src/main/java/com/example/omisetest/charity/presentation/DonationFragment.kt
@@ -5,6 +5,8 @@ import android.view.View
 import com.example.omisetest.R
 import com.example.omisetest.databinding.FragmentDonationBinding
 import com.example.omisetest.presentation.DataBindingFragment
+import kotlinx.android.synthetic.main.fragment_donation.*
+import android.text.InputFilter
 
 class DonationFragment : DataBindingFragment<FragmentDonationBinding>() {
 
@@ -13,6 +15,9 @@ class DonationFragment : DataBindingFragment<FragmentDonationBinding>() {
   lateinit var eventHandler: DonationEventHandler
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    eventHandler = DonationEventHandler(activity!!)
+    eventHandler = DonationEventHandler(activity!!, card_number, cvv_code, expired_date, card_holder)
+    card_number.addTextChangedListener(eventHandler.cardNumberWatcher)
+    card_holder.filters = arrayOf<InputFilter>(InputFilter.AllCaps())
+    expired_date.addTextChangedListener(eventHandler.expiryWatcher)
   }
 }
